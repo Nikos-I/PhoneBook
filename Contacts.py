cd93e24f20895426ff45509dcac219b905949705
Nikos-I <nik.norht@gmail.com>
2022-10-20 14:38:23 +0300

    Contacts черновик 5

--- Contacts.py
+++ Contacts.py
@@ -4,9 +4,12 @@ import sqlite3
 import functools 
 import operator
 
-pk_sel = 0
-
+# Обработка события выбора из списка и передача данных в поля ввода
 def lst_person_selected(event):
+    global pk_sel
+    global cur
+    global conn
+    lst_person.focus()
     sel_str = lst_person.get((lst_person.curselection())[0])
     pk_sel = int(sel_str[0:sel_str.find(".",0)])
     cur.execute(f"select lastname, firstname, patronymic from persons where id_person = {pk_sel};")
@@ -15,22 +18,46 @@ def lst_person_selected(event):
     text_fn.set(firstname)
     text_pn.set(patronymic)
     
-
+# Удаление контакта
 def btn_del_cont_command():
-    print("command")
+    global pk_sel
+    global cur
+    global conn
+    
+    exec_str = f'DELETE from persons  WHERE id_person = {pk_sel}'
+    print(exec_str)
+    cur.execute(exec_str) 
+    conn.commit()   
+    get_list_person()
 
-
+# Сохранение изменений контакта
 def btn_save_con_command():
-    print(ent_lastname.get())
-    print(ent_firstname.get())
-    print(ent_patronymic.get())
-    print("command")
-    cur.execute(f"select lastname, firstname, patronymic from persons where id_person = {pk_sel};")    
-    print(f'UPDATE persons set lastname="{ent_lastname.get()}", firstname="{ent_firstname.get()}", patronymic="{ent_patronymic.get()}" WHERE id_person = {pk_sel}') 
+    global pk_sel
+    global cur
+    global conn
 
+    exec_str = f'UPDATE persons set lastname="{ent_lastname.get()}", firstname="{ent_firstname.get()}", patronymic="{ent_patronymic.get()}" WHERE id_person = {pk_sel}'
+    print(exec_str) 
+    cur.execute(exec_str) 
+    conn.commit()   
+    get_list_person()
+    lst_person.focus()
+ 
+# Создание нового контакта
 def btn_add_cont_command():
-    print("command")
-
+    global pk_sel
+    global cur
+    global conn
+    exec_str = 'SELECT max(id_person)+1 FROM Persons'
+    cur.execute(exec_str) 
+    new_id = functools.reduce(operator.add,(cur.fetchone()))
+    print(exec_str) 
+    exec_str = f'INSERT INTO persons (id_person, lastname, firstname, patronymic) VALUES ({new_id}, "{ent_lastname.get()}", "{ent_firstname.get()}", "{ent_patronymic.get()}")'
+    print(exec_str) 
+    cur.execute(exec_str) 
+    conn.commit()   
+    get_list_person()
+    
 
 def btn_del_phone_command():
     print("command")
@@ -39,15 +66,33 @@ def btn_del_phone_command():
 def btn_save_phone_command():
     print("command")
 
+# Обновление списка контактов
+def get_list_person():
+    global cur
+    global list_person
+    global person_var
+    cur.execute("select title from v_person_short;")
+    list_person = functools.reduce(operator.add,(cur.fetchall()))
+    person_var.set(list_person) 
+
+pk_sel = 0
+# sel_str = ''
+list_person = []
+
+# Установка соединения с БД
 conn = sqlite3.connect('d:/GDisk/GeekBraims/Python/PythonSeminars/Seminar7/PhoneBook/db/phone_book.db')
 cur = conn.cursor()
-cur.execute("select title from v_person_short;")
-list_person = functools.reduce(operator.add,(cur.fetchall()))
-        
+# get_list_person()
+# cur.execute("select title from v_person_short;")
+
+# Создание головного окла        
 root = tk.Tk()
+
+person_var = tk.Variable()
 text_fn = tk.StringVar()
 text_ln = tk.StringVar()
 text_pn = tk.StringVar()
+get_list_person()
 
 #setting title
 root.title("Телефонный справочник")
@@ -69,6 +114,7 @@ lbl_title["text"] = "Контакты"
 lbl_title["relief"] = "flat"
 lbl_title.place(x=100,y=10,width=70,height=25)
 
+# Листбокс контактов
 lst_person=tk.Listbox(root)
 lst_person["borderwidth"] = "3px"
 ft = tkFont.Font(family='Serif',size=10)
@@ -78,11 +124,10 @@ lst_person["justify"] = "left"
 person_var = tk.Variable(value=list_person)
 lst_person["listvariable"] = person_var
 lst_person["selectmode"] = tk.SINGLE
-lst_person.bind("<<ListboxSelect>>", lst_person_selected)
-
-
+lst_person.bind("<<ListboxSelect>>", lst_person_selected) # Привязка события Select
 lst_person.place(x=30,y=40,width=219,height=386)
 
+# Кнопки контактов
 btn_add_cont=tk.Button(root)
 btn_add_cont["bg"] = "#f0f0f0"
 ft = tkFont.Font(family='Serif',size=10)
@@ -121,6 +166,8 @@ lbl_lastname["justify"] = "right"
 lbl_lastname["text"] = "Фамилия"
 lbl_lastname.place(x=260,y=80,width=70,height=25)
 
+
+# Поля ввода
 ent_lastname=tk.Entry(root)
 ent_lastname["borderwidth"] = "2px"
 ft = tkFont.Font(family='Serif',size=10)
@@ -129,6 +176,7 @@ ent_lastname["fg"] = "#333333"
 ent_lastname["justify"] = "left"
 ent_lastname["text"] = "Entry"
 ent_lastname["textvariable"] = text_ln
+# lst_person.bind("<<ListboxSelect>>", lst_person_selected)
 ent_lastname.place(x=340,y=80,width=250,height=30)
 
 
@@ -150,7 +198,6 @@ ent_firstname["justify"] = "left"
 ent_firstname["text"] = "Entry"
 ent_firstname["textvariable"] = text_fn
 ent_firstname.place(x=340,y=130,width=245,height=30)
-
 
 
 lbl_patronymic=tk.Label(root)
@@ -179,23 +226,16 @@ lbl_phone_num["justify"] = "right"
 lbl_phone_num["text"] = "Телефоны"
 lbl_phone_num.place(x=260,y=230,width=70,height=25)
 
-lbl_phone_num=tk.Listbox(root)
-lbl_phone_num["borderwidth"] = "2px"
+# Список телефонов
+lst_phone_num=tk.Listbox(root)
+lst_phone_num["borderwidth"] = "2px"
 ft = tkFont.Font(family='Serif',size=10)
-lbl_phone_num["font"] = ft
-lbl_phone_num["fg"] = "#333333"
-lbl_phone_num["justify"] = "center"
-lbl_phone_num.place(x=340,y=230,width=247,height=91)
+lst_phone_num["font"] = ft
+lst_phone_num["fg"] = "#333333"
+lst_phone_num["justify"] = "center"
+lst_phone_num.place(x=340,y=230,width=247,height=91)
 
-btn_del_phone=tk.Button(root)
-btn_del_phone["bg"] = "#f0f0f0"
-ft = tkFont.Font(family='Serif',size=10)
-btn_del_phone["font"] = ft
-btn_del_phone["fg"] = "#000000"
-btn_del_phone["justify"] = "center"
-btn_del_phone["text"] = "Удалить"
-btn_del_phone.place(x=360,y=340,width=70,height=25)
-btn_del_phone["command"] = btn_del_phone_command
+# Кнопки телефонов
 
 btn_save_phone=tk.Button(root)
 btn_save_phone["bg"] = "#f0f0f0"
@@ -207,6 +247,16 @@ btn_save_phone["text"] = "Сохранить"
 btn_save_phone.place(x=490,y=340,width=70,height=25)
 btn_save_phone["command"] = btn_save_phone_command
 
+btn_del_phone=tk.Button(root)
+btn_del_phone["bg"] = "#f0f0f0"
+ft = tkFont.Font(family='Serif',size=10)
+btn_del_phone["font"] = ft
+btn_del_phone["fg"] = "#000000"
+btn_del_phone["justify"] = "center"
+btn_del_phone["text"] = "Удалить"
+btn_del_phone.place(x=360,y=340,width=70,height=25)
+btn_del_phone["command"] = btn_del_phone_command
+
 # GMessage_847=tk.Message(root)
 # ft = tkFont.Font(family='Serif',size=10)
 # GMessage_847["font"] = ft
@@ -214,13 +264,5 @@ btn_save_phone["command"] = btn_save_phone_command
 # GMessage_847["justify"] = "center"
 # GMessage_847["text"] = "Ошибка!"
 # GMessage_847.place(x=200,y=470,width=80,height=25)
-
-
-
-
- 
-
-
-
 
 root.mainloop()
